b8a17295385484237df7bb7441d05d984ea6c006
Eric <ericmitchell1625@hotmail.com>
2023-12-05 19:25:25 -0500

    fixtures added, comment_app and post_app models created

--- back-end/comment_app/models.py
+++ back-end/comment_app/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from post_app.models import Post
+from user_app.models import User
 
 
 class Comment(models.Model):
-    pass
+    post_id = models.ForeignKey(Post, related_name='post', on_delete=models.CASCADE, null=True, blank=True)
+    content = models.TextField(blank=True, null=True)
+    comment_user = models.ForeignKey(User, related_name='comment_user', on_delete=models.CASCADE, blank=True, null=True)
--- back-end/post_app/models.py
+++ back-end/post_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from user_app.models import User
 
 class Post(models.Model):
-    pass
+    poster = models.ForeignKey(User, related_name='user', blank=True, null=True, on_delete=models.CASCADE)
+    post_content = models.TextField(null=True, blank=True)
