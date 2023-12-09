6315cbcd433b411c7aa13d29e544c0653f1bdcfe
eric <ericmitchell1625@hotmail.com>
2023-12-08 19:38:20 -0500

    workgin on filter

--- back-end/post_app/serializers.py
+++ back-end/post_app/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import Post
-
+from user_app.serializers import UserSerializer
 
 class PostSerializer(ModelSerializer):
-    poster = SerializerMethodField()
+    poster = UserSerializer(many=True)
+
     class Meta:
         model = Post
         fields = ['poster', 'post_content']
 
-    def get_poster(self, obj):
-        return str(obj.poster.display_name)
+    # def get_poster(self, obj):
+    #     return str(obj.poster.display_name)
     
--- back-end/post_app/views.py
+++ back-end/post_app/views.py
@@ -1,8 +1,22 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import Post, PostSerializer
+from user_app.serializers import User, UserSerializer
 
 class All_posts(APIView):
     def get(self, request):
+        # user_target = request.user.target_language
+        # p = Post.objects.filter()
+        # print(p.poster.native_language)
+
         posts = PostSerializer(Post.objects.order_by('id'), many=True)
+        # return Response(user.data)
+        # data = {
+        #     "posts" : posts.data,
+        #     "user": user.data
+        # }
         return Response(posts.data)
+    
+
+
+    ## use object.filter(target_language="")
