71aa4317b3323a071bc5834eacc8ffeffb1b3ee6
Eric <ericmitchell1625@hotmail.com>
2023-12-15 17:32:17 -0500

    working on css

--- front-end/src/components/UserCheck.jsx
+++ front-end/src/components/UserCheck.jsx
@@ -5,9 +5,12 @@ const UserCheck = () => {
     const navigate = useNavigate()
     return(
         <>
+        <div id='login-container'>
+            <div className='login-box'>
             <h1>Welcome to Tiki Taka</h1>
             <h2>Please log in or create an account</h2>
-            <div>
+            </div>
+            <div className='login-box'>
             <Button variant="primary" size="lg" active onClick={()=>navigate("/login/")}>
                   Login
              </Button>{' '}
@@ -15,6 +18,7 @@ const UserCheck = () => {
                 Create Account
             </Button>{' '}
             </div>
+        </div>
         </>
     )
 }
